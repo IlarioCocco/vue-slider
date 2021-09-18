d68db88918887b6e98dc220321e16487ec11d8af
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-18 02:30:21 +0200

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@ const app = new Vue(
     {
         el: "#root",
         data: {
-
+            image: "https://content.skyscnr.com/m/46d2bafcbe21ca44/original/Cala-Goloritze-Sardegna-GettyImages-894583274.jpg",
+            
         },
         methods: {
 
